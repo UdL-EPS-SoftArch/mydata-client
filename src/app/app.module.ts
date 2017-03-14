6a0cf0987e304e2897f0f8486754990498430e93
Roberto García <roberto@rhizomik.net>
2017-03-14 20:29:52 +0100

    Register dataset list and details components and routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,22 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { RouterModule } from "@angular/router";
+import { RouterModule } from '@angular/router';
 
-import { routes } from "./app.routing";
+import { routes } from './app.routing';
 import { AppComponent } from './app.component';
 import { DatasetFormComponent } from './dataset/dataset-form/dataset-form.component';
 import { AboutComponent } from './about/about.component';
-import { DatasetService } from "./dataset/dataset.service";
-import { LoginBasicModule } from "./login-basic/login-basic.module";
-import { AuthenticationBasicService } from "./login-basic/authentication-basic.service";
-import { LoggedInGuard } from "./login-basic/loggedin.guard";
+import { DatasetService } from './dataset/dataset.service';
+import { LoginBasicModule } from './login-basic/login-basic.module';
+import { AuthenticationBasicService } from './login-basic/authentication-basic.service';
+import { LoggedInGuard } from './login-basic/loggedin.guard';
+import { DatasetsListComponent } from './dataset/datasets-list/datasets-list.component';
+import { DatasetDetailsComponent } from './dataset/dataset-details/dataset-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DatasetFormComponent,
-    AboutComponent
+    AboutComponent,
+    DatasetsListComponent,
+    DatasetDetailsComponent
   ],
   imports: [
     BrowserModule,
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,9 +1,13 @@
-import { Routes } from "@angular/router";
-import { DatasetFormComponent } from "./dataset/dataset-form/dataset-form.component";
-import { AboutComponent } from "./about/about.component";
+import { Routes } from '@angular/router';
+import { DatasetFormComponent } from './dataset/dataset-form/dataset-form.component';
+import { AboutComponent } from './about/about.component';
+import { DatasetDetailsComponent } from './dataset/dataset-details/dataset-details.component';
+import { DatasetsListComponent } from './dataset/datasets-list/datasets-list.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'about', pathMatch: 'full' },
   { path: 'about', component: AboutComponent },
-  { path: 'datasets/new', component: DatasetFormComponent }
+  { path: 'datasets', component: DatasetsListComponent },
+  { path: 'datasets/new', component: DatasetFormComponent },
+  { path: 'datasets/:id', component: DatasetDetailsComponent }
 ];
