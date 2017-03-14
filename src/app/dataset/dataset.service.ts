431c9356ed83367dcf07fb7ac686269014ed0ab5
Roberto García <roberto@rhizomik.net>
2017-03-14 20:31:52 +0100

    Add getDataset to DatasetService

--- src/app/dataset/dataset.service.ts
+++ src/app/dataset/dataset.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
-import { Http, Response, Headers, RequestOptions } from "@angular/http";
-import { AuthenticationBasicService } from "../login-basic/authentication-basic.service";
-import { Observable } from "rxjs";
-import { Dataset } from "./dataset";
-import { environment } from "../../environments/environment";
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
+import { AuthenticationBasicService } from '../login-basic/authentication-basic.service';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/catch';
+import { Dataset } from './dataset';
+import { environment } from '../../environments/environment';
 
 @Injectable()
 export class DatasetService {
@@ -18,12 +19,19 @@ export class DatasetService {
       .catch((error: any) => Observable.throw(error.json()));
   }
 
+  // GET /datasets/id
+  getDataset(uri: string): Observable<Dataset> {
+    return this.http.get(`${environment.API}${uri}`)
+      .map((res: Response) => new Dataset(res.json()))
+      .catch((error: any) => Observable.throw(error.json()));
+  }
+
   // POST /datasets
   addDataset(dataset: Dataset): Observable<Dataset> {
-    let body = JSON.stringify(dataset);
-    let headers = new Headers({ 'Content-Type': 'application/json' });
+    const body = JSON.stringify(dataset);
+    const headers = new Headers({ 'Content-Type': 'application/json' });
     headers.append('Authorization', this.authentication.getCurrentUser().authorization);
-    let options = new RequestOptions({ headers: headers });
+    const options = new RequestOptions({ headers: headers });
 
     return this.http.post(`${environment.API}/datasets`, body, options)
       .map((res: Response) => new Dataset(res.json()))
