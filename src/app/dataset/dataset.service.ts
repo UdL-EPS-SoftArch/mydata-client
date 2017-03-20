a1da8d4e389773cd6dd8ac9feb047a6b5e49719c
yatan <yatan87@gmail.com>
2017-03-20 16:46:40 +0100

    Search by description works manually, need refactor component

--- src/app/dataset/dataset.service.ts
+++ src/app/dataset/dataset.service.ts
@@ -37,4 +37,12 @@ export class DatasetService {
       .map((res: Response) => new Dataset(res.json()))
       .catch((error: any) => Observable.throw(error.json()));
   }
+
+  // GET /datasets/ + search/findByDescriptionContaining?description
+  getDatasetByDescriptionWords(keyword: string): Observable<Dataset[]> {
+    console.log(environment.API + "/datasets/search/findByDescriptionContaining?description=" + keyword);
+    return this.http.get(environment.API + "/datasets/search/findByDescriptionContaining?description=" + keyword)
+      .map((res: Response) => res.json()._embedded.datasets.map(json => new Dataset(json)))
+      .catch((error: any) => Observable.throw(error.json()));
+  }
 }
--- src/app/dataset/datasets-list/datasets-list.component.ts
+++ src/app/dataset/datasets-list/datasets-list.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { DatasetService } from '../dataset.service';
-import { Dataset } from '../dataset';
+import {Component, OnInit} from '@angular/core';
+import {DatasetService} from '../dataset.service';
+import {Dataset} from '../dataset';
 
 @Component({
   selector: 'app-datasets-list',
@@ -11,12 +11,26 @@ export class DatasetsListComponent implements OnInit {
   public datasets: Dataset[] = [];
   public errorMessage: string;
 
-  constructor(private datasetService: DatasetService) { }
+  constructor(private datasetService: DatasetService) {
+  }
 
   ngOnInit() {
     this.datasetService.getAllDatasets().subscribe(
-      datasets => { this.datasets = datasets; },
+      datasets => {
+        this.datasets = datasets;
+      },
       error => this.errorMessage = <any>error.message
     );
   }
+
+  performSearch(searchTerm: string): void {
+    console.log(`User entered: ${searchTerm}`);
+    this.datasetService.getDatasetByDescriptionWords(searchTerm).subscribe(
+      datasets => {
+        this.datasets = datasets;
+      },
+      error => this.errorMessage = <any>error.message
+    );
+  }
+
 }
