bb2e40a6f943b585803b6bf52b9a86da29f7319a
Roberto García <roberto@rhizomik.net>
2017-03-23 13:22:14 +0100

    Add field uri to dataset

--- src/app/dataset/dataset.ts
+++ src/app/dataset/dataset.ts
@@ -1,11 +1,12 @@
 export class Dataset {
+  uri: string;
   title: string;
   description: string;
   dateTime: string;
   lastModified: string;
   blocked = false;
   flags = 0;
-  _links: any;
+  _links: any = {};
 
   constructor(values: Object = {}) {
     (<any>Object).assign(this, values);
