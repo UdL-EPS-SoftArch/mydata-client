97bc8fcc744f97eb0e9bf98275a73910dfcb32d2
joanj94 <joanj_94@hotmail.com>
2017-03-27 17:33:28 +0200

    - Added function getAllTags and fixed at tag.service.spec.ts (Issue #14)

--- src/app/tag/tag.service.spec.ts
+++ src/app/tag/tag.service.spec.ts
@@ -13,11 +13,11 @@ describe('TagService', () => {
 
   const tag1 = new Tag({
     'uri': '/tags/1',
-    'title': 'Tag 1',
+    'name': 'Tag 1',
   });
   const tag2 = new Tag({
     'uri': '/tags/2',
-    'title': 'Tag 2',
+    'name': 'Tag 2',
   });
 
   beforeEach(async(() => {
@@ -34,5 +34,26 @@ describe('TagService', () => {
       imports: [HttpModule]
     });
   }));
+
+  describe('#getAllTags()', () => {
+    it('should return all tags',
+      inject([ MockBackend, TagService ], fakeAsync((mockBackend, service) => {
+        const apiResponse = new ResponseOptions({
+          body: {
+            _embedded: {
+              schemas: [ tag1, tag2 ]}}});
+
+        mockBackend.connections.subscribe((connection: MockConnection) => {
+          expect(connection.request.url).toBe('http://localhost:8080/tags');
+          connection.mockRespond(new Response(apiResponse));
+        });
+
+        service.getAllTags().subscribe((data) => {
+          expect(data.length).toBe(2);
+          expect(data[0].title).toEqual(tag1.name);
+          expect(data[1].title).toEqual(tag2.name);
+        });
+      })));
+  });
 
 });
