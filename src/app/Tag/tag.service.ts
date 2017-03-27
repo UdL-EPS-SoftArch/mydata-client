89be6519f5dccfa459f0d9ca5116ff5afd8a9891
joanj94 <joanj_94@hotmail.com>
2017-03-27 16:44:04 +0200

    - Created Tag Service tag.service.ts (Issue #14) - Added function getAllTags at tag.service.ts (Issue #14)

---
+++ src/app/Tag/tag.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
+import { AuthenticationBasicService } from '../login-basic/authentication-basic.service';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/observable/throw';
+import { environment } from '../../environments/environment';
+import {Tag} from "@angular/compiler/src/i18n/serializers/xml_helper";
+
+@Injectable()
+export class TagService {
+
+  constructor(private http: Http,
+              private authentication: AuthenticationBasicService) {
+  }
+
+  // GET /tags
+  getAllTags(): Observable<Tag[]> {
+    return this.http.get(`${environment.API}/tags`)
+      .map((res: Response) => res.json()._embedded.schemas.map(json => new Tag(json)))
+      .catch((error: any) => Observable.throw(error.json()));
+  }
+}
