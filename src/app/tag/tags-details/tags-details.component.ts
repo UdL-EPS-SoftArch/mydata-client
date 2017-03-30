2e03d47e6ffb6d45f0e4c5032b5e1aecf0093527
Franco Friz Rodriguez <ffriz2010@gmail.com>
2017-03-30 16:22:07 +0200

    - Created file tags-details.component.ts (Issue #14)

---
+++ src/app/tag/tags-details/tags-details.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import {TagService} from "../tag.service";
+import {Tag} from "../tag";
+
+@Component({
+  selector: 'app-tag-details',
+  templateUrl: './tag-details.component.html',
+  styleUrls: ['./tag-details.component.css']
+})
+export class TagDetailsComponent implements OnInit {
+  public tag: Tag = new Tag();
+  public errorMessage: string;
+
+  constructor(private route: ActivatedRoute,
+              private schemaService: TagService) { }
+
+  ngOnInit() {
+    this.route.params
+      .map(params => params['id'])
+      .subscribe((id) => {
+        const uri = `/tags/${id}`;
+        this.schemaService.getTag(uri).subscribe(
+          tag => { this.tag = tag; },
+          error => this.errorMessage = <any>error.message
+        );
+      });
+  }
+}
