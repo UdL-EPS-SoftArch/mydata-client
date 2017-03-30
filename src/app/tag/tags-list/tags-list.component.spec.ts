c3d8f33e05d863f9305c60e72b7da29cca61f8ad
Franco Friz Rodriguez <ffriz2010@gmail.com>
2017-03-30 15:58:03 +0200

    - Fixed specification at: tags-list.component.spec.ts (Issue #14)

--- src/app/tag/tags-list/tags-list.component.spec.ts
+++ src/app/tag/tags-list/tags-list.component.spec.ts
@@ -1,16 +1,18 @@
-import {async, TestBed, inject, fakeAsync} from '@angular/core/testing';
+import {async, TestBed, inject, fakeAsync, ComponentFixture} from '@angular/core/testing';
 
 import { TagsListComponent } from './tags-list.component';
 import { TagService } from '../tag.service';
 import { Tag } from '../tag';
-import {MockBackend, MockConnection} from "@angular/http/testing";
-import {ResponseOptions, Response} from "@angular/http";
 import {AppComponent} from "../../app.component";
 import {MockTagService} from "../../../test/mocks/tag.service";
 import {RouterTestingModule} from "@angular/router/testing";
 import {NO_ERRORS_SCHEMA} from "@angular/core";
+import {Router} from "@angular/router";
 
 describe('TagsListComponent', () => {
+  let component: TagsListComponent;
+  let fixture: ComponentFixture<TagsListComponent>;
+
   const tag1 = new Tag({
     'uri': '/tags/Tag1',
     "name": 'Tag1',
@@ -31,24 +33,25 @@ describe('TagsListComponent', () => {
     });
   }));
 
-  describe('#getAllTags()', () => {
-    it('should fetch and render all tags', async(
-      inject([ MockBackend, TagService ], fakeAsync((mockBackend, service) => {
-        const apiResponse = new ResponseOptions({
-          body: {
-            _embedded: {
-              tags: [ tag1, tag2 ]}}});
+  it('should fetch and render all tags', async(
+    inject([Router, Location, TagService], (router, location, service) => {
+      TestBed.createComponent(AppComponent);
+      service.setResponse([tag1, tag2]);
 
-        mockBackend.connections.subscribe((connection: MockConnection) => {
-          expect(connection.request.url).toBe('http://localhost:8080/tags');
-          connection.mockRespond(new Response(apiResponse));
-        });
+      router.navigate(['/tags']).then(() => {
+        expect(location.path()).toBe('/tags');
+        expect(service.getAllTags).toHaveBeenCalled();
 
-        service.getAllTags().subscribe((data) => {
-          expect(data.length).toBe(2);
-          expect(data[0].name).toEqual(tag1.name);
-          expect(data[1].name).toEqual(tag2.name);
-        });
-      }))));
-  });
+        fixture = TestBed.createComponent(TagsListComponent);
+        fixture.detectChanges();
+        component = fixture.debugElement.componentInstance;
+        expect(component.tags[0].name).toBe('Tag1');
+        expect(component.tags[1].name).toBe('Tag2');
+
+        const compiled = fixture.debugElement.nativeElement;
+        expect(compiled.querySelectorAll('.panel-heading')[0].innerHTML).toContain('Tag1');
+        expect(compiled.querySelectorAll('.panel-heading')[1].innerHTML).toContain('Tag2');
+      });
+    })
+  ));
 });
