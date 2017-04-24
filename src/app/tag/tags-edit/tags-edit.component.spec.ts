19ad9acc85db300d3722094c08fb52e90510a68a
joanj94 <joanj_94@hotmail.com>
2017-04-24 15:55:03 +0200

    - Fixed import errors, changed " for ' at tags-edit.component(.spec).ts (Issue #26)

--- src/app/tag/tags-edit/tags-edit.component.spec.ts
+++ src/app/tag/tags-edit/tags-edit.component.spec.ts
@@ -1,15 +1,15 @@
-import {async, ComponentFixture, inject, TestBed} from "@angular/core/testing";
-import {TagEditComponent} from "./tags-edit.component";
-import {Tag} from "../tag";
-import {AppComponent} from "../../app.component";
-import {TagDetailsComponent} from "../tags-details/tags-details.component";
-import {RouterTestingModule} from "@angular/router/testing";
-import {TagService} from "../tag.service";
-import {MockTagService} from "../../../test/mocks/tag.service";
-import {FormsModule, ReactiveFormsModule} from "@angular/forms";
-import {NO_ERRORS_SCHEMA} from "@angular/core";
-import {LoggedInGuard} from "../../login-basic/loggedin.guard";
-import {Router} from "@angular/router";
+import {async, ComponentFixture, inject, TestBed} from '@angular/core/testing';
+import {TagEditComponent} from './tags-edit.component';
+import {Tag} from '../tag';
+import {AppComponent} from '../../app.component';
+import {TagDetailsComponent} from '../tags-details/tags-details.component';
+import {RouterTestingModule} from '@angular/router/testing';
+import {TagService} from '../tag.service';
+import {MockTagService} from '../../../test/mocks/tag.service';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {NO_ERRORS_SCHEMA} from '@angular/core';
+import {LoggedInGuard} from '../../login-basic/loggedin.guard';
+import {Router} from '@angular/router';
 import { Location } from '@angular/common';
 
 describe('TagEditComponent', () => {
--- src/app/tag/tags-edit/tags-edit.component.ts
+++ src/app/tag/tags-edit/tags-edit.component.ts
@@ -1,8 +1,8 @@
-import {Component, OnInit} from "@angular/core";
-import {Tag} from "../tag";
-import {AbstractControl, FormBuilder, FormGroup, Validators} from "@angular/forms";
-import {ActivatedRoute, Router} from "@angular/router";
-import {TagService} from "../tag.service";
+import {Component, OnInit} from '@angular/core';
+import {Tag} from '../tag';
+import {AbstractControl, FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {ActivatedRoute, Router} from '@angular/router';
+import {TagService} from '../tag.service';
 
 @Component({
   selector: 'app-tag-edit',
