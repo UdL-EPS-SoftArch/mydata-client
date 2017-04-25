f32136ab8fce9458aa5a2875fd26836b12bb8550
Xavi <xaviermoyafarre@gmail.com>
2017-04-25 16:20:20 +0200

    features and pages complete

--- e2e/pages/field-form-page.ts
+++
@@ -1,3 +0,0 @@
-/**
- * Created by Xavi on 25/4/17.
- */
--- e2e/pages/fields-list.page.ts
+++ e2e/pages/fields-list.page.ts
@@ -1,3 +1,23 @@
-/**
- * Created by Xavi on 25/4/17.
- */
+import { element, by, ElementArrayFinder, ElementFinder } from 'protractor';
+import { promise } from 'selenium-webdriver';
+
+export class FieldsListPage {
+
+  private fields: ElementArrayFinder;
+
+  constructor() {
+    this.fields = this.getFields();
+  }
+
+  getFields(): ElementArrayFinder {
+    return element.all(by.css('div.panel'));
+  }
+
+  getFieldInPosition(position: number): ElementFinder {
+    return this.fields.get(position - 1);
+  }
+
+  getFieldsCount(): promise.Promise<number> {
+    return this.fields.count();
+  }
+}
