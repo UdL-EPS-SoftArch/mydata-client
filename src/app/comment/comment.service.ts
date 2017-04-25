b4b2fa80aaa97b8907b6947179cc124e58f1044c
Santi Gatius <sgv1@alumnes.udl.cat>
2017-04-25 16:29:10 +0200

    Comment & comment.service file

--- src/app/comment/comment.service.ts
+++ src/app/comment/comment.service.ts
@@ -1,3 +1,76 @@
-/**
- * Created by santi on 25/04/17.
- */
+import {AuthenticationBasicService} from "../login-basic/authentication-basic.service";
+import {Http, Response, Headers, RequestOptions} from "@angular/http";
+import {Observable} from "rxjs";
+import {environment} from "../../environments/environment";
+import { Comment } from './comment';
+import {Dataset} from "../dataset/dataset";
+
+
+export class CommentService{
+
+  constructor(private http: Http,
+              private authentication: AuthenticationBasicService) {}
+
+  // GET /comments
+  getAllComments(): Observable<Comment[]> {
+    return this.http.get(`${environment.API}/datasets`)
+      .map((res: Response) => res.json()._embedded.comments.map(json => new Comment(json)))
+      .catch((error: any) => Observable.throw(error.json()));
+  }
+
+  // GET /comments/id
+  getComment(uri: string): Observable<Comment> {
+    return this.http.get(`${environment.API}${uri}`)
+      .map((res: Response) => new Comment(res.json()))
+      .catch((error: any) => Observable.throw(error.json()));
+  }
+
+  // GET /comments/OrderById
+  getAllCommentsOrderedById(): Observable<Comment[]> {
+    return this.http.get(`${environment.API}/datasets?sort=id`)
+      .map((res: Response) => res.json()._embedded.comments.map(json => new Comment(json)))
+      .catch((error: any) => Observable.throw(error.json()));
+  }
+
+  // GET /comments/ + search/findByDatasetContaining?dataset
+  getCommentByDataset(dataset: Dataset): Observable<Comment[]> {
+    return this.http.get(environment.API + '/comments/search/findByDatasetContaining?dataset=' + dataset)
+      .map((res: Response) => res.json()._embedded.comments.map(json => new Comment(json)))
+      .catch((error: any) => Observable.throw(error.json()));
+  }
+
+  // POST /comments
+  addComment(comment: Comment): Observable<Comment> {
+    const body = JSON.stringify(comment);
+    const headers = new Headers({ 'Content-Type': 'application/json' });
+    headers.append('Authorization', this.authentication.getCurrentUser().authorization);
+    const options = new RequestOptions({ headers: headers });
+
+    return this.http.post(`${environment.API}/comments`, body, options)
+      .map((res: Response) => new Comment(res.json()))
+      .catch((error: any) => Observable.throw(error.json()));
+  }
+
+  // UPDATE /comments/id
+  updateComment(comment: Comment): Observable<Comment> {
+    const body = JSON.stringify(comment);
+    const headers = new Headers({ 'Content-Type': 'application/json' });
+    headers.append('Authorization', this.authentication.getCurrentUser().authorization);
+    const options = new RequestOptions({ headers: headers });
+
+    return this.http.put(`${environment.API}${comment.uri}`, body, options)
+      .map((res: Response) => new Comment(res.json()))
+      .catch((error: any) => Observable.throw(error.json()));
+  }
+
+  // DELETE /comment/{id}
+  deleteComment(comment: Comment): Observable<Response> {
+    const headers = new Headers({'Content-Type': 'application/json'});
+    headers.append('Authorization', this.authentication.getCurrentUser().authorization);
+    const options = new RequestOptions({headers: headers});
+
+    return this.http.delete(environment.API + comment.uri, options)
+      .map((res: Response) => res)
+      .catch((error: any) => Observable.throw(error.json()));
+  }
+}
--- src/app/comment/comment.ts
+++ src/app/comment/comment.ts
@@ -1,3 +1,18 @@
+import {Dataset} from "../dataset/dataset";
+import {User} from "../login-basic/user";
 /**
  * Created by santi on 25/04/17.
  */
+export class Comment{
+  uri: string;
+  id: string;
+  text: string;
+  dataset: Dataset;
+  user: User;
+  dateTime: string;
+  _links: any = {};
+
+  constructor(values: Object = {}){
+    (<any>Object).assign(this, values);
+  }
+}
