cb258b9ed454009af0999374b9fb8b06a4f3b57c
Sergi Grau Dalmau <sergi-grau9@hotmail.com>
2017-05-03 15:25:32 +0200

    To add a schema instance to dataset details

--- src/app/dataset/dataset-details/dataset-details.component.ts
+++ src/app/dataset/dataset-details/dataset-details.component.ts
@@ -4,6 +4,9 @@ import { DatasetService } from '../dataset.service';
 import { Dataset } from '../dataset';
 import { AuthenticationBasicService } from '../../login-basic/authentication-basic.service';
 import { DatasetOwnerService } from '../../user/dataset-owner.service';
+import { Schema } from '../../schema/schema';
+import { SchemaService } from '../../schema/schema.service';
+import { SchemaOwnerService } from '../../user/schema-owner.service';
 
 @Component({
   selector: 'app-dataset-details',
@@ -12,12 +15,15 @@ import { DatasetOwnerService } from '../../user/dataset-owner.service';
 })
 export class DatasetDetailsComponent implements OnInit {
   public dataset: Dataset = new Dataset();
+  public schema: Schema = new Schema();
   public errorMessage: string;
   public isOwner: boolean;
 
   constructor(private route: ActivatedRoute,
               private router: Router,
               private datasetService: DatasetService,
+              private schemaService: SchemaService,
+              private schemaOwnerService: SchemaOwnerService,
               private authenticationService: AuthenticationBasicService,
               private datasetOwnerService: DatasetOwnerService) { }
 
@@ -33,12 +39,30 @@ export class DatasetDetailsComponent implements OnInit {
               this.datasetOwnerService.getDatasetOwner(this.dataset._links.owner.href).subscribe(
                 owner => {
                   this.isOwner = this.authenticationService.getCurrentUser().username === owner.getUserName();
-              });
+                });
             }
           },
           error => this.errorMessage = <any>error.message,
         );
       });
+    this.route.params
+      .map(params => params['id'])
+      .subscribe((id) => {
+        const uri = `/datasets/${id}/schema`;
+        this.schemaService.getSchema(uri).subscribe(
+          schema => {
+            this.schema = schema;
+            if (this.schema._links != null) {
+              this.schemaOwnerService.getSchemaOwner(this.schema._links.owner.href).subscribe(
+                owner => {
+                  this.isOwner = this.authenticationService.getCurrentUser().username === owner.getUserName();
+                });
+            }
+          },
+          error => this.errorMessage = <any>error.message
+        );
+      });
+
   }
 
   onDelete(dataset) {
