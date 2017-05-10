cacabb4b45f59525539b696cffb079cab517e3ed
Roberto García <roberto@rhizomik.net>
2017-05-11 00:22:19 +0200

    Code for POST and DELETE API calls in the service

--- src/app/dataset/datafile/datafile.service.ts
+++ src/app/dataset/datafile/datafile.service.ts
@@ -1,15 +1,17 @@
-import {Http, Response} from '@angular/http';
-import {Observable} from 'rxjs/Observable';
-import {environment} from '../../../environments/environment';
-import {DataFile} from './datafile';
-import {Injectable} from '@angular/core';
+import { Http, RequestOptions, Response, Headers } from '@angular/http';
+
+import { Observable } from 'rxjs/Observable';
+import { environment } from '../../../environments/environment';
+import { DataFile } from './datafile';
+import { Injectable } from '@angular/core';
+import { AuthenticationBasicService } from '../../login-basic/authentication-basic.service';
 
 @Injectable()
 export class DataFileService {
 
-  constructor(private http: Http) {
+  constructor(private http: Http,
+              private authentication: AuthenticationBasicService) {
   }
-
 
   // GET /dataFiles/OrderByTitle
   getAllDataFilesOrderedByTitle(): Observable<DataFile[]> {
@@ -18,7 +20,6 @@ export class DataFileService {
       .catch((error: any) => Observable.throw(error.json()));
   }
 
-
   // GET /dataFiles/id
   getDataFile(uri: string): Observable<DataFile> {
     return this.http.get(`${environment.API}${uri}`)
@@ -26,11 +27,33 @@ export class DataFileService {
       .catch((error: any) => Observable.throw(error.json()));
   }
 
-  // GET /datafiles
+  // GET /dataFiles
   getAllDataFiles(): Observable<DataFile[]> {
     return this.http.get(`${environment.API}/dataFiles`)
       .map((res: Response) => res.json()._embedded.dataFiles.map(json => new DataFile(json)))
       .catch((error: any) => Observable.throw(error.json()));
   }
 
+  // POST /dataFiles
+  addDataFile(dataFile: DataFile): Observable<DataFile> {
+    const body = JSON.stringify(dataFile);
+    const headers = new Headers({'Content-Type': 'application/json'});
+    headers.append('Authorization', this.authentication.getCurrentUser().authorization);
+    const options = new RequestOptions({headers: headers});
+
+    return this.http.post(`${environment.API}/dataFiles`, body, options)
+      .map((res: Response) => new DataFile(res.json()))
+      .catch((error: any) => Observable.throw(error.json()));
+  }
+
+  // DELETE /dataFile/{id}
+  deleteDataFile(dataFile: DataFile): Observable<Response> {
+    const headers = new Headers({'Content-Type': 'application/json'});
+    headers.append('Authorization', this.authentication.getCurrentUser().authorization);
+    const options = new RequestOptions({headers: headers});
+
+    return this.http.delete(environment.API + dataFile.uri, options)
+      .map((res: Response) => res)
+      .catch((error: any) => Observable.throw(error.json()));
+  }
 }
