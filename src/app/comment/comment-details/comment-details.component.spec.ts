5b4f3a283194b3f331e76b09c67b998e36f99103
Chaimae El Morabet Lachkar <elmchai401@gmail.com>
2017-05-14 17:43:25 +0200

    Created and finished package comment-details

---
+++ src/app/comment/comment-details/comment-details.component.spec.ts
@@ -0,0 +1,55 @@
+import { ComponentFixture, async, TestBed, inject } from '@angular/core/testing';
+import { CommentDetailsComponent } from './comment-details.component';
+import { Comment } from '../comment';
+import { AppComponent } from '../../app.component';
+import { CommentService } from '../comment.service';
+import { MockCommentService } from '../../../test/mocks/comment.service';
+import { RouterTestingModule } from '@angular/router/testing';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { Router } from '@angular/router';
+import { Location } from '@angular/common';
+
+describe('CommentDetailsComponent', () => {
+  let fixture: ComponentFixture<CommentDetailsComponent>;
+  let component: CommentDetailsComponent;
+
+  const comment1 = new Comment({
+    'uri': '/comments/1',
+    'text': 'First comment'
+  });
+  const comment2 = new Comment({
+    'uri': '/comments/2',
+    'text': 'Second comment'
+  });
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ AppComponent, CommentDetailsComponent ],
+      providers: [ { provide: CommentService, useClass: MockCommentService } ],
+      imports: [ RouterTestingModule.withRoutes([
+        { path: 'comments/:id', component: CommentDetailsComponent }
+      ])],
+      schemas: [ NO_ERRORS_SCHEMA ]
+    });
+  }));
+
+  it('should fetch and render the requested comment', async(
+    inject([Router, Location, CommentService], (router, location, service) => {
+      TestBed.createComponent(AppComponent);
+      service.setResponse(comment1);
+
+      router.navigate(['/comments/1']).then(() => {
+        expect(location.path()).toBe('/comments/1');
+        expect(service.getComment).toHaveBeenCalledWith('/comments/1');
+
+        fixture = TestBed.createComponent(CommentDetailsComponent);
+        fixture.detectChanges();
+        component = fixture.debugElement.componentInstance;
+        expect(component.comment.text).toBe('comment1');
+
+        const compiled = fixture.debugElement.nativeElement;
+        expect(compiled.querySelectorAll('p')[0].innerHTML).toBe('comment1');
+      });
+    })
+  ));
+});
