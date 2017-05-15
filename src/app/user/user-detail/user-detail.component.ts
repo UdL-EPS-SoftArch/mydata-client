5973adbbd6b8788bb9a2b689dc04067e3415a9c2
Pau <pbalaguer19@gmail.com>
2017-05-15 16:35:16 +0200

    User Datasets listed. #52

--- src/app/user/user-detail/user-detail.component.ts
+++ src/app/user/user-detail/user-detail.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {User} from '../user';
 import {ActivatedRoute} from '@angular/router';
 import {UserService} from '../user.service';
+import {Dataset} from '../../dataset/dataset';
 
 @Component({
   selector: 'app-user-detail',
@@ -10,6 +11,7 @@ import {UserService} from '../user.service';
 })
 export class UserDetailComponent implements OnInit {
   public user: User = new User();
+  public datasets: Dataset[] = [];
   public errorMessage: string;
 
   constructor(private route: ActivatedRoute,
@@ -25,11 +27,13 @@ export class UserDetailComponent implements OnInit {
         this.userService.getUser(uri).subscribe(
           user => {
             this.user = user;
+            this.getUserInfo(this.user);
           },
           error => {
             this.userService.getUser(uri2).subscribe(
               user => {
                 this.user = user;
+                this.getUserInfo(this.user);
               },
               error2 => {
                 this.errorMessage = <any>error2.message;
@@ -40,4 +44,16 @@ export class UserDetailComponent implements OnInit {
       });
   }
 
+  getUserInfo (user: User): void {
+    console.log(user.authorities[0].authority);
+    console.log(user._links.sharedDatasets.href);
+    console.log(user._links.ownsDatasets.href);
+    console.log(user._links.ownsSchemas.href);
+    console.log(user._links.ownsLicenses.href);
+    console.log(user.uri);
+    this.userService.getUserDatasets(user.uri + '/ownsDatasets').subscribe(
+      datasets => {
+        this.datasets = datasets;
+      });
+  }
 }
--- src/app/user/user.service.ts
+++ src/app/user/user.service.ts
@@ -6,6 +6,7 @@ import 'rxjs/add/observable/throw';
 import { Http, Response } from '@angular/http';
 import {User} from './user';
 import {environment} from '../../environments/environment';
+import {Dataset} from '../dataset/dataset';
 
 @Injectable()
 export class UserService {
@@ -15,6 +16,12 @@ export class UserService {
   getUser(uri: any): Observable<User> {
     return this.http.get(`${environment.API}${uri}`)
       .map((res: Response) => new User(res.json()))
+      .catch((error: any) => Observable.throw(error.json()));
+  }
+
+  getUserDatasets(uri: any): Observable<Dataset[]> {
+    return this.http.get(`${environment.API}${uri}`)
+      .map((res: Response) => res.json()._embedded.datasets.map(json => new Dataset(json)))
       .catch((error: any) => Observable.throw(error.json()));
   }
 }
