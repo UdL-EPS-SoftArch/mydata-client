1df58fd61730ca9ca526b4d084c8385863f2c50d
gerard87 <gerar.f87@gmail.com>
2017-05-16 16:45:00 +0200

    Add onDelete and owner to datafile details component

--- src/app/dataset/datafile-list/datafile-details.component.ts
+++ src/app/dataset/datafile-list/datafile-details.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { DataFileService } from '../datafile/datafile.service';
 import { DataFile } from '../datafile/datafile';
 import { AuthenticationBasicService } from '../../login-basic/authentication-basic.service';
+import { DatasetOwnerService } from '../../user/dataset-owner.service';
 
 
 @Component({
@@ -13,11 +14,13 @@ export class DatafileDetailsComponent implements OnInit {
   public datafile: DataFile = new DataFile();
   public errorMessage: string;
   public isOwner: boolean;
+  public ownerName: string;
 
   constructor(private route: ActivatedRoute,
               private router: Router,
               private datafileService: DataFileService,
-              private authenticationService: AuthenticationBasicService) { }
+              private authenticationService: AuthenticationBasicService,
+              private datasetOwnerService: DatasetOwnerService) { }
 
   ngOnInit() {
     this.route.params
@@ -27,10 +30,24 @@ export class DatafileDetailsComponent implements OnInit {
         this.datafileService.getDataFile(uri).subscribe(
           datafile => {
             this.datafile = datafile;
+            if (this.datafile._links != null) {
+              this.datasetOwnerService.getDatasetOwner(this.datafile._links.owner.href).subscribe(
+                owner => {
+                  this.ownerName = owner.getUserName();
+                  this.isOwner = this.authenticationService.getCurrentUser().username === owner.getUserName();
+                });
+            }
           },
           error => this.errorMessage = <any>error.message,
         );
       });
+  }
+
+  onDelete(datafile) {
+    this.datafileService.deleteDataFile(datafile).subscribe(
+      response => { this.router.navigate(['/dataFiles']); },
+      error => this.errorMessage = <any>error.message,
+    );
   }
 
 }
