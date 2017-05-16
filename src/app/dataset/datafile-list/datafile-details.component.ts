f2aeaf630e02d5249815a06d75a11176a59397e7
Sergi Grau Dalmau <sergi-grau9@hotmail.com>
2017-05-16 15:52:09 +0200

    Show Schema to DataFile

--- src/app/dataset/datafile-list/datafile-details.component.ts
+++ src/app/dataset/datafile-list/datafile-details.component.ts
@@ -3,6 +3,8 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { DataFileService } from '../datafile/datafile.service';
 import { DataFile } from '../datafile/datafile';
 import { AuthenticationBasicService } from '../../login-basic/authentication-basic.service';
+import { SchemaService } from '../../schema/schema.service';
+import { Schema } from '../../schema/schema';
 
 
 @Component({
@@ -11,12 +13,14 @@ import { AuthenticationBasicService } from '../../login-basic/authentication-bas
 })
 export class DatafileDetailsComponent implements OnInit {
   public datafile: DataFile = new DataFile();
+  public schema: Schema = new Schema();
   public errorMessage: string;
   public isOwner: boolean;
 
   constructor(private route: ActivatedRoute,
               private router: Router,
               private datafileService: DataFileService,
+              private schemaService: SchemaService,
               private authenticationService: AuthenticationBasicService) { }
 
   ngOnInit() {
@@ -27,6 +31,12 @@ export class DatafileDetailsComponent implements OnInit {
         this.datafileService.getDataFile(uri).subscribe(
           datafile => {
             this.datafile = datafile;
+            const uri_schema = `/datasets/${id}/schema`;
+            this.schemaService.getSchema(uri_schema).subscribe(
+              schema => {
+                this.schema = schema;
+
+              })
           },
           error => this.errorMessage = <any>error.message,
         );
