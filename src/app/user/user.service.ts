bf565d8ffbfb27fe4d28fe49d031bdfe643fba2f
Pau <pbalaguer19@gmail.com>
2017-06-06 11:20:50 +0200

    User service pagination methods added

--- src/app/user/user.service.ts
+++ src/app/user/user.service.ts
@@ -10,6 +10,7 @@ import {Dataset} from '../dataset/dataset';
 import {Schema} from '../schema/schema';
 import {OpenLicense} from '../license/open-license/open-license';
 import {ClosedLicense} from '../license/closed-license/closed-license';
+import {PageWrapper} from '../pageWrapper';
 
 @Injectable()
 export class UserService {
@@ -29,9 +30,33 @@ export class UserService {
       .catch((error: any) => Observable.throw(error.json()));
   }
 
+  getUserDatasetsPaginated(uri: any): Observable<PageWrapper> {
+    return this.http.get(`${environment.API}${uri}`)
+      .map((res: Response) => {
+        const pw = new PageWrapper();
+        const data = res.json();
+        pw.elements = data._embedded.datasets.map(json => new Dataset(json));
+        pw.pageInfo = data.page;
+        return pw;
+      })
+      .catch((error: any) => Observable.throw(error.json()));
+  }
+
   getUserSchemas(uri: any): Observable<Schema[]> {
     return this.http.get(`${environment.API}${uri}`)
       .map((res: Response) => res.json()._embedded.schemas.map(json => new Schema(json)))
+      .catch((error: any) => Observable.throw(error.json()));
+  }
+
+  getUserSchemasPaginated(uri: any): Observable<PageWrapper> {
+    return this.http.get(`${environment.API}${uri}`)
+      .map((res: Response) => {
+        const pw = new PageWrapper();
+        const data = res.json();
+        pw.elements = data._embedded.schemas.map(json => new Schema(json));
+        pw.pageInfo = data.page;
+        return pw;
+      })
       .catch((error: any) => Observable.throw(error.json()));
   }
 
@@ -46,6 +71,18 @@ export class UserService {
       .catch((error: any) => Observable.throw(error.json()));
   }
 
+  getUserOpenLicensePaginated(uri: any): Observable<PageWrapper> {
+    return this.http.get(`${environment.API}${uri}`)
+      .map((res: Response) => {
+        const pw = new PageWrapper();
+        const data = res.json();
+        pw.elements = data._embedded.openLicenses.map(json => new OpenLicense(json));
+        pw.pageInfo = data.page;
+        return pw;
+      })
+      .catch((error: any) => Observable.throw(error.json()));
+  }
+
   getUserClosedLicenses(uri: any): Observable<ClosedLicense[]> {
     return this.http.get(`${environment.API}${uri}`)
       .map((res: Response) => {
@@ -53,6 +90,18 @@ export class UserService {
         if (data.closedLicenses) {
           return res.json()._embedded.closedLicenses.map(json => new ClosedLicense(json));
         }
+      })
+      .catch((error: any) => Observable.throw(error.json()));
+  }
+
+  getUserClosedLicensePaginated(uri: any): Observable<PageWrapper> {
+    return this.http.get(`${environment.API}${uri}`)
+      .map((res: Response) => {
+        const pw = new PageWrapper();
+        const data = res.json();
+        pw.elements = data._embedded.closedLicenses.map(json => new ClosedLicense(json));
+        pw.pageInfo = data.page;
+        return pw;
       })
       .catch((error: any) => Observable.throw(error.json()));
   }
